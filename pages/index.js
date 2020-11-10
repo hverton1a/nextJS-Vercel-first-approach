59476edcfea52206b063492284b5837b8b18cc6d
hverton1a <hverton1a@gmail.com>
2020-11-10 18:02:58 -0300

    primria pagina

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import{useState} from 'react';
+
+
+const Hello =()=>{
+    return (<h2>Hello World</h2>)
+}
+
+function Home(){
+    return     <div> 
+         <h2>Home 1{Contador.contador}</h2>
+         <Contador />
+         </div>
+}
+
+
+const Contador = ()=>{
+    const [contador, setContador]=useState(1)
+
+    const adicionarContador=()=>{
+        setContador(contador+1);
+    }
+
+    return(
+        <div className="Counter-wrapper">
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+
+
+export default Home;
